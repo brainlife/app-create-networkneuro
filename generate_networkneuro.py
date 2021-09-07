ce1bf1738d3167983f9cf044c6a58e156991aef5
Brad Caron <bacaron@iu.edu>
2021-09-07 12:14:38 -0400

    reason: file name fix

--- generate_networkneuro.py
+++ generate_networkneuro.py
@@ -103,8 +103,8 @@ def generate_networkneuro():
 
 		# store node names
 		tmp = {}
-		tmp['roi1'] = int(ridx1)
-		tmp['roi2'] = int(ridx2)
+		tmp['roi1'] = [ int(f['label']) for f in labels if f['voxel_value'] == ridx1 ]
+		tmp['roi2'] = [ int(f['label']) for f in labels if f['voxel_value'] == ridx2 ]
 		
 		# grab weights
 		tmp['weights'] = {}
